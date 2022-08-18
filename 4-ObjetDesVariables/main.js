dcd00185231a399c16f5a36086f7d3b62409703c
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-18 19:53:37 +0200

    objet des variables et scopes

---
+++ 4-ObjetDesVariables/main.js
@@ -0,0 +1,6 @@
+ /* L'objet des variables contient ces trois choses dans l'ordre:
+    1-Les arguments de la fonction.
+    2-Les déclarations de fonctions.
+    3-Les déclarations de variables.
+  
+ */
---
+++ 5-ChaîneDesScopes/main.js
@@ -0,0 +1,6 @@
+/*: La règle qu'il faut absolument retenir c'est:
+    .Une fonction enfant peut accéder au scope de ses parents.
+     
+ */
+
+    
